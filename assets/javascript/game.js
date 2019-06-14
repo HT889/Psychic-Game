c426e222e76b174b9af6eb01d292d5333dc595f4
Haig Talarian <haigtalarian@gmail.com>
2019-06-13 22:11:53 -0700

    wins losses guesses js done, computer guesses done

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+console.log(computerChoice);
+
+// User guesses by pressing a key,
+
+document.onkeypress = function (){
+    var playerGuess = event.key;
+
+    if(playerGuess === computerChoice){
+        wins++;
+        guesses = 10;
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }else{
+        guesses--;
+    }
+    if(guesses === 0){
+        losses++;
+        guesses = 10;
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    console.log('player guess:' + playerGuess)
+    console.log('w: ' + wins)
+console.log(losses)
+console.log(guesses)
+console.log(computerChoice)
+
+}
